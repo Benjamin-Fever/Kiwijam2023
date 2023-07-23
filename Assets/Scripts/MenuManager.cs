9e2ae31a32233cae283f76bf0533002044cf1835
Benjamin Fever <ben.fever07@gmail.com>
2023-07-23 12:54:01 +1200

    Basic Menu Implementation

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public void OnStartClick()
+    {
+
+    }
+
+    public void OnTutorialClick()
+    {
+        
+    }
+
+    public void OnCreditsClick()
+    {
+
+    }
+}
